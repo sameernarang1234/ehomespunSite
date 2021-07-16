11b5f2491562e73ad37957b9dc4f81b90f2f37df
sameernarang1234 <samre1234@gmail.com>
2021-07-16 16:22:33 +0530

    Login Page added

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    path('login/', views.loginPage, name='loginPage'),
 ]
--- WebApp/views.py
+++ WebApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homePage(request):
     return render(request, 'home.html')
+
+def loginPage(request):
+    return render(request, 'login.html')
